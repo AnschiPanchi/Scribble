06e5511093ad26b08d10742f942cced963d49832
Ansh Gupta <anshg1948@gmail.com>
2026-04-05 10:13:01 +0530

    deploying changes

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
+  'https://scribble-mu-seven.vercel.app', // Deployed App
   process.env.CLIENT_URL
 ].filter(Boolean);
 
